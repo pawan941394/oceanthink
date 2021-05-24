c83dbe87682d178f394ccf0498ac237dcb4605dd
pawan941394 <63099276+pawan941394@users.noreply.github.com>
2021-05-24 17:08:29 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,169 @@
+/**
+* Template Name: Mentor - v4.1.0
+* Template URL: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    });
+  }
+
+  /**
+   * Testimonials slider
+   */
+  new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+
+      1200: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      }
+    }
+  });
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+
+})()
+
+const navbar = document.querySelector(".header");
+const commentbox = document.querySelector(".comment1");
+const commentbox1 = document.querySelector(".comment2");
+const commentbox2 = document.querySelector(".comment3");
+const commentbox3 = document.querySelector(".comment4");
+
+window.onscroll = ()=>{
+  this.scrollY > 20 ?  navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+}
+
+function commentOpen(){
+  commentbox.classList.add("is-hidden");
+  commentbox1.classList.remove("is-hidden")
+}
+function commentClose(){
+  commentbox1.classList.add("is-hidden");
+  commentbox.classList.remove("is-hidden")
+}
+function commentOpen1(){
+  commentbox2.classList.add("is-hidden");
+  commentbox3.classList.remove("is-hidden")
+}
+function commentClose1(){
+  commentbox3.classList.add("is-hidden");
+  commentbox2.classList.remove("is-hidden")
+}
+
+function theme1(){
+  document.getElementById('cls').className = "theme1"
+}
+function theme2(){
+  document.getElementById('cls').className = "theme2"
+}
+function theme3(){
+  document.getElementById('cls').className = "theme3"
+}
+function theme4(){
+  document.getElementById('cls').className = "theme4"
+}
